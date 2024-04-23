960fe362641ddc8622e92888a927c6e76dbb94f5
Rufino John <aguilarufino@gmail.com>
2024-04-23 14:24:58 +0800

    added delete routine

--- app/Views/pages/offices.php
+++ app/Views/pages/offices.php
@@ -220,7 +220,41 @@
                     delay: 3000
                 });
             },
-        })
+        });
+
+    });
+
+
+    $(document).on("click", "#deleteBtn", function() {
+        let row = $(this).parents("tr")[0];
+        let id = table.row(row).data().id;
+
+        if (confirm("Are you sure you want to delete this item?")) {
+            $.ajax({
+                url: "<?= base_url('offices'); ?>/" + id,
+                type: "DELETE",
+                success: function(response) {
+                    $(document).Toasts('create', {
+                        class: 'bg-success',
+                        title: 'SUCCESS',
+                        body: JSON.stringify(response.message),
+                        autohide: true,
+                        delay: 3000
+                    });
+                    table.ajax.reload();
+                },
+                error: function(response) {
+                    let parseresponse = JSON.parse(response.responseText);
+                    $(document).Toasts('create', {
+                        class: 'bg-danger',
+                        title: 'ERROR',
+                        body: JSON.stringify(parseresponse.message),
+                        autohide: true,
+                        delay: 3000
+                    });
+                },
+            });
+        }
 
     });
 
--- app/Views/template/menu.php
+++ app/Views/template/menu.php
@@ -1,4 +1,4 @@
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light bg-lime">
 
     <ul class="navbar-nav">
         <li class="nav-item">
@@ -18,9 +18,9 @@
 </nav>
 
 
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-lime elevation-4">
 
-    <a href="index3.html" class="brand-link">
+    <a href="index3.html" class="brand-link bg-danger">
         <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
         <span class="brand-text font-weight-light">CIAPP</span>
     </a>
@@ -50,19 +50,19 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
-                    <a href="<?= base_url('dashboard')?>" class="nav-link">
+                    <a href="<?= base_url('dashboard')?>" class="nav-link <?= (uri_string() =='dashboard') ? 'active' : '' ?>">
                         <i class="fas fa-chart-pie nav-icon"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?= base_url('tickets')?>" class="nav-link">
+                    <a href="<?= base_url('tickets')?>" class="nav-link <?= (uri_string() =='tickets') ? 'active' : '' ?>">
                         <i class="fas fa-file nav-icon"></i>
                         <p>Tickets</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?= base_url('offices')?>" class="nav-link">
+                    <a href="<?= base_url('offices')?>" class="nav-link <?= (uri_string() =='offices') ? 'active' : '' ?>">
                         <i class="fas fa-briefcase nav-icon"></i>
                         <p>Offices</p>
                     </a>
